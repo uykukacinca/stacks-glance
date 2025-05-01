b1246d84928e40446b9a0832d909f86cd7b9f9d2
Atlas <17718755+uykukacinca@users.noreply.github.com>
2025-05-01 06:23:45 +0300

    up

--- shared/assets/script.js
+++ shared/assets/script.js
@@ -9,26 +9,31 @@ document.addEventListener(
       anchor.click();
     }
 
-    if (classList.contains("glance-copy")) {
+    if (classList.contains("glimpse-copy")) {
+      const textData = target.dataset.text;
       const textDiv = target.previousElementSibling;
 
-      if (textDiv && textDiv.classList.contains("text")) {
-        const text = textDiv.textContent;
+      if (textData || (textDiv && textDiv.classList.contains("text"))) {
+        const text = textData || textDiv.textContent;
         navigator.clipboard
           .writeText(text)
           .then(() => {
             console.log("Text copied to clipboard:", text);
-            target.classList.add("color-positive");
-            setTimeout(() => {
-              target.classList.remove("color-positive");
-            }, 1500);
+            if (textDiv) {
+              target.classList.add("color-positive");
+              setTimeout(() => {
+                target.classList.remove("color-positive");
+              }, 1500);
+            }
           })
           .catch((err) => {
             console.error("Failed to copy text:", err);
-            target.classList.add("color-negative");
-            setTimeout(() => {
-              target.classList.remove("color-negative");
-            }, 1500);
+            if (textDiv) {
+              target.classList.add("color-negative");
+              setTimeout(() => {
+                target.classList.remove("color-negative");
+              }, 1500);
+            }
           });
       }
     }
